c218bd4c0877ff8544829ac3f6d2f9f9cd862c30
yachoo2606 <janbialy@hotmail.com>
2024-03-04 14:54:31 +0100

    Initial commit

---
+++ src/main/java/org/example/masterprotocolworker/MasterProtocolWorkerApplication.java
@@ -0,0 +1,13 @@
+package org.example.masterprotocolworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterProtocolWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterProtocolWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/masterprotocolworker/MasterProtocolWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.masterprotocolworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterProtocolWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
